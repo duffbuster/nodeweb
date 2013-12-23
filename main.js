b801a44684430aa4bd6545b67a1bfed669fb6834
Colin Mackey <colin@fishbrains.com>
2013-12-23 15:44:58 -0500

    nothing much

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("what's UP");
+	res.end("what's UP, this is raw code bra");
 });
 server.listen(8080);
