0cb0d14b420ad18b682d93a6b3e9c6a3c0ea514b
Colin Mackey <colin@fishbrains.com>
2013-12-23 16:08:38 -0500

    fixed azure port

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var http = require('http');
-var fs = require('fs');
+// var fs = require('fs');
+var port = process.env.PORT || 1337;
 // var index = fs.readFileSync('index.html');
 
 var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end("what's UP, this is raw code bra");
 });
-server.listen(8080);
+server.listen(port);
