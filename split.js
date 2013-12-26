8423aef2efe9b5e8516ed51e0a64f8e92bcccf6d
Colin Mackey <colin@fishbrains.com>
2013-12-26 12:44:27 -0500

    fixed it

--- split.js
+++ split.js
@@ -1,7 +1,11 @@
 var split = require('split');
 var through = require('through');
-process.stdin
-	.pipe(split())
-	.pipe(through(function (line) {
-		console.log(line[2]);
-	}));
+
+var lineCount = 0;
+
+var tr = through(function (buf) {
+	var line = buf.toString();
+	this.queue(lineCount % 2 === 0 ? line.toLowerCase() + '\n' : line.toUpperCase() + '\n');
+	lineCount++;
+});
+process.stdin.pipe(split()).pipe(tr).pipe(process.stdout);
